40b70671c935d441236984742cc83337be284393
Daniel Bevan <20234823+Dbevan770@users.noreply.github.com>
2024-02-17 15:29:06 +0100

    "Added vim-tmux navigation plugin to allow movement from neovim to non neovim windows in tmux"

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,5 +1,11 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Vim Tmux Navigator
+vim.keymap.set("n", "<C-h>", "<CMD>TmuxNavigateLeft<CR>", { silent = true, noremap = true, desc = "Window left" })
+vim.keymap.set("n", "<C-j>", "<CMD>TmuxNavigateDown<CR>", { silent = true, noremap = true, desc = "Window down" })
+vim.keymap.set("n", "<C-k>", "<CMD>TmuxNavigateUp<CR>", { silent = true, noremap = true, desc = "Window up" })
+vim.keymap.set("n", "<C-l>", "<CMD>TmuxNavigateRight<CR>", { silent = true, noremap = true, desc = "Window right" })
 
 -- Move selection up or down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -17,20 +23,20 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- Paste over while preserving clipboard
-vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("x", "<leader>p", '"_dP')
 
 -- Yank to system clipboard
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
 
 -- Delete to void register (gets thrown out)
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
+vim.keymap.set("n", "<leader>d", '"_d')
+vim.keymap.set("v", "<leader>d", '"_d')
 
 -- DON'T EVER PRESS CAPITAL Q
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set('n', '<Space>', '<nop>', { silent = true })
+vim.keymap.set("n", "<Space>", "<nop>", { silent = true })
 
 -- Create a new tmux session
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux new tmux-sessionizer<CR>")
@@ -42,14 +48,14 @@ vim.keymap.set("n", "<leader>ln", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>lp", "<cmd>lprev<CR>zz")
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 -- Change windows
 -- Left
@@ -62,32 +68,31 @@ vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 -- Github CLI mappings
-vim.keymap.set('n', '<leader>ghrcp', function()
-  vim.ui.input({
-    prompt = 'Enter the name of the repo: '
-  },
-    function(input)
-      vim.cmd(string.format("!gh repo create %s --public", input))
-    end)
+vim.keymap.set("n", "<leader>ghrcp", function()
+	vim.ui.input({
+		prompt = "Enter the name of the repo: ",
+	}, function(input)
+		vim.cmd(string.format("!gh repo create %s --public", input))
+	end)
 end, {
-    desc = 'GitHub repo create (Public)'
-  })
+	desc = "GitHub repo create (Public)",
+})
 
 -- Highlight on yank
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
 })
 
 -- Dotnet mappings
-vim.keymap.set('n', '<leader>nr', '<CMD>!dotnet run<CR>', {
-  desc = 'Dot[N]et [R]un'
+vim.keymap.set("n", "<leader>nr", "<CMD>!dotnet run<CR>", {
+	desc = "Dot[N]et [R]un",
 })
 
-vim.keymap.set('n', '<leader>nb', '<CMD>!dotnet build<CR>', {
-  desc = 'Dot[N]et [B]uild'
+vim.keymap.set("n", "<leader>nb", "<CMD>!dotnet build<CR>", {
+	desc = "Dot[N]et [B]uild",
 })
